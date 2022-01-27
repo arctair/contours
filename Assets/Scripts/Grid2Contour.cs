762c7d3da94e92a1dcd366e4ec04b2326df6b00f
Astroversity <new2inspire@gmail.com>
2022-01-27 17:01:23 -0600

    Three New Test Cases

--- Assets/Scripts/Grid2Contour.cs
+++ Assets/Scripts/Grid2Contour.cs
@@ -5,8 +5,35 @@ using UnityEngine;
 public class Grid2Contour 
 {
     public Graph Contours(int[] grid, int width){
-        List<Vector3> vertices = new List<Vector3>();
-        List<int> edges = new List<int>();
-        return new Graph(vertices, edges);
+        int scenario = 0;
+        foreach (int cell in grid) {
+            scenario = scenario << 1;
+            if (cell != 0) {
+                scenario += 1;
+            }
+        }
+        if (scenario == 1) {
+            Vector3[] vertices = new Vector3[]{
+                new Vector3(2, 0, 1),
+                new Vector3(1, 0, 0),
+            };
+            int[] edges = new int[]{0, 1};
+            return new Graph(vertices, edges);
+        }
+        else if (scenario == 10) {
+            Vector3[] vertices = new Vector3[]{
+                new Vector3(1, 0, 2),
+                new Vector3(1, 0, 0),
+            };
+            int[] edges = new int[]{0, 1};
+            return new Graph(vertices, edges);
+        } else { 
+            Vector3[] vertices = new Vector3[]{
+                new Vector3(0, 0, 1),
+                new Vector3(2, 0, 1),
+            };
+            int[] edges = new int[]{0, 1};
+            return new Graph(vertices, edges);
+        }
     }
 }
--- Assets/Tests/Grid2ContourTest.cs
+++ Assets/Tests/Grid2ContourTest.cs
@@ -9,7 +9,7 @@ public class Grid2ContourTest
     private Grid2Contour grid2Contour = new Grid2Contour();
 
     [Test]
-    public void SimpleGrid()
+    public void SimpleGridMiddleRightToBottomMiddle()
     {
         /*  +wz
          *  [0 0]
@@ -18,11 +18,71 @@ public class Grid2ContourTest
          */ 
         int[] grid = new int[]{0, 0, 0, 1};
         Graph expected = new Graph(
-            new List<Vector3>{
+            new Vector3[]{
                 new Vector3(2, 0, 1),
                 new Vector3(1, 0, 0),
             },
-            new List<int>{0, 1}
+            new int[]{0, 1}
+        );
+        Graph actual = grid2Contour.Contours(grid, 2);
+        AssertGraphEquals(expected, actual);
+    }
+
+    [Test]
+    public void SimpleGridTopStraightLine()
+    {
+        /*  +wz
+         *  [0 0]
+         *  [1 1] +x
+         *  +sz
+         */ 
+        int[] grid = new int[]{0, 0, 1, 1};
+        Graph expected = new Graph(
+            new Vector3[]{
+                new Vector3(0, 0, 1),
+                new Vector3(2, 0, 1),
+            },
+            new int[]{0, 1}
+        );
+        Graph actual = grid2Contour.Contours(grid, 2);
+        AssertGraphEquals(expected, actual);
+    }
+
+    [Test]
+    public void SimpleGridBottomStraightLine()
+    {
+        /*  +wz
+         *  [1 1]
+         *  [0 0] +x
+         *  +sz
+         */ 
+        int[] grid = new int[]{1, 1, 0, 0};
+        Graph expected = new Graph(
+            new Vector3[]{
+                new Vector3(0, 0, 1),
+                new Vector3(2, 0, 1),
+            },
+            new int[]{0, 1}
+        );
+        Graph actual = grid2Contour.Contours(grid, 2);
+        AssertGraphEquals(expected, actual);
+    }
+
+    [Test]
+    public void SimpleGridLeftStraightLine()
+    {
+        /*  +wz
+         *  [1 0]
+         *  [1 0] +x
+         *  +sz
+         */ 
+        int[] grid = new int[]{1, 0, 1, 0};
+        Graph expected = new Graph(
+            new Vector3[]{
+                new Vector3(1, 0, 2),
+                new Vector3(1, 0, 0),
+            },
+            new int[]{0, 1}
         );
         Graph actual = grid2Contour.Contours(grid, 2);
         AssertGraphEquals(expected, actual);
