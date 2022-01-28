9b1d1160d5ed9265757408fca288dde2307efd2e
tyler <tyler@cruftbusters.com>
2022-01-28 12:28:46 -0600

    extract duplicate contour edges as constants

--- Assets/Scripts/Grid2Contour.cs
+++ Assets/Scripts/Grid2Contour.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Grid2Contour
 {
+    private static int[] EDGE_01 = new int[] { 0, 1 };
+
+    private static int[] EDGES_0123 = new int[] { 0, 1, 2, 3 };
+
+    private static int[] EDGES_0213 = new int[] { 0, 2, 1, 3 };
+
     public Graph
     Contours(float tl, float tr, float bl, float br, float threshold)
     {
@@ -19,28 +25,28 @@ public class Grid2Contour
                         new Vector3(2, 0, 1),
                         new Vector3(1, 0, 0)
                     },
-                    new int[] { 0, 1 });
+                    EDGE_01);
             case 2:
             case 13:
                 return new Graph(new Vector3[] {
                         new Vector3(0, 0, 1),
                         new Vector3(1, 0, 0)
                     },
-                    new int[] { 0, 1 });
+                    EDGE_01);
             case 4:
             case 11:
                 return new Graph(new Vector3[] {
                         new Vector3(1, 0, 2),
                         new Vector3(2, 0, 1)
                     },
-                    new int[] { 0, 1 });
+                    EDGE_01);
             case 7:
             case 8:
                 return new Graph(new Vector3[] {
                         new Vector3(1, 0, 2),
                         new Vector3(0, 0, 1)
                     },
-                    new int[] { 0, 1 });
+                    EDGE_01);
             case 6:
             case 9:
                 float average = (tl + tr + bl + br) / 4;
@@ -51,22 +57,22 @@ public class Grid2Contour
                         new Vector3(1, 0, 0)
                     },
                     average > threshold ^ scenario == 6
-                        ? new int[] { 0, 2, 1, 3 }
-                        : new int[] { 0, 1, 2, 3 });
+                        ? EDGES_0213
+                        : EDGES_0123);
             case 3:
             case 12:
                 return new Graph(new Vector3[] {
                         new Vector3(0, 0, 1),
                         new Vector3(2, 0, 1)
                     },
-                    new int[] { 0, 1 });
+                    EDGE_01);
             case 5:
             case 10:
                 return new Graph(new Vector3[] {
                         new Vector3(1, 0, 2),
                         new Vector3(1, 0, 0)
                     },
-                    new int[] { 0, 1 });
+                    EDGE_01);
             default:
                 return new Graph(new Vector3[0], new int[0]);
         }
